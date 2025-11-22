146c8e4f4bea0a9eac3e3580fa322c51b72bd50d
Gabriel Azorín <115559474+GaboAzorin@users.noreply.github.com>
2025-11-22 18:50:46 -0300

    Actualizar scraper.py

--- scraper.py
+++ scraper.py
@@ -13,8 +13,8 @@ CSV_FILE = 'LOTO_HISTORIAL_MAESTRO.csv'
 STATUS_FILE = 'system_status.json'
 
 # SEMILLA 2024 (29 Dic - Sorteo 5210)
-HISTORY_START_DATE = datetime.datetime(2024, 12, 29)
-HISTORY_START_SORTEO = 5210
+HISTORY_START_DATE = datetime.datetime(2024, 12, 31)
+HISTORY_START_SORTEO = 5211
 
 def log(msg, status="INFO"):
     ts = datetime.datetime.now().strftime("%H:%M:%S")
