1d87ee4bc5ba8c33e2b84aaca73168221f9a5d77
Gabriel Azorín <115559474+GaboAzorin@users.noreply.github.com>
2025-11-22 17:11:38 -0300

    Actualizar scraper.py
    ajuste

--- scraper.py
+++ scraper.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 CSV_FILE = 'LOTO_HISTORIAL_MAESTRO.csv'
 STATUS_FILE = 'system_status.json'
 
-HISTORY_START_DATE = datetime.datetime(2025, 1, 9)
-HISTORY_START_SORTEO = 5215
+HISTORY_START_DATE = datetime.datetime(2024, 12, 26)
+HISTORY_START_SORTEO = 5209
 
 def log(msg, status="INFO"):
     ts = datetime.datetime.now().strftime("%H:%M:%S")
