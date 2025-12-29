45d043a3c85043c5f5cbdd43668e0cdefc142222
GaboAzorin <ga.azorin@gmail.com>
2025-12-29 10:22:46 -0300

    Corrección de texto

--- support/tests/crear_zip.py
+++ support/tests/crear_zip.py
@@ -141,7 +141,7 @@ def comprimir_para_gemini():
 
     print("Además, ya está en tu portapapeles el texto de resumen del proyecto :)")
     
-    texto_para_portapapeles = 'Arquitectura MLOps Serverless orientada a eventos ejecutada sobre GitHub Actions. El sistema implementa un pipeline ETL autónomo (Playwright) con reconstrucción temporal y manejo de concurrencia mediante colas asíncronas (UUID tickets) con consolidación batch para garantizar la integridad atómica de los datos (*_MAESTRO.csv).\nEl núcleo de inferencia opera mediante un ensamble dinámico (RandomForest, Cadenas de Markov, Heurísticas) orquestado por un algoritmo de votación ponderada (Consenso Meritocrático). Integra un ciclo de retroalimentación cerrado (RL-lite): el agente Juez calcula la función de pérdida sobre predicciones pasadas y el Entrenador ajusta los pesos sinápticos en loto_genome.json en tiempo de ejecución.'
+    texto_para_portapapeles = 'Arquitectura MLOps Serverless orientada a eventos ejecutada sobre GitHub Actions. El sistema implementa un pipeline ETL autónomo (Playwright) con reconstrucción temporal y manejo de concurrencia mediante colas asíncronas (UUID tickets) con consolidación batch para garantizar la integridad atómica de los datos (*_MAESTRO.csv).\nEl núcleo de inferencia opera mediante un ensamble dinámico (RandomForest, Cadenas de Markov, Heurísticas) orquestado por un algoritmo de votación ponderada (Consenso Meritocrático). Integra un ciclo de retroalimentación cerrado (RL-lite): el agente Juez calcula la función de pérdida sobre predicciones pasadas y el Entrenador ajusta los pesos sinápticos en loto_genome.json en tiempo de ejecución.\n\nNecesito que lo analices muy en profunidad.'
     pyperclip.copy(texto_para_portapapeles)
     print(texto_para_portapapeles)
 
